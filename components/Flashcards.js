3ed5160f569acf628a2591f91852151defc0bbe6
Robson Dias <robson@binariio.com>
2018-03-06 17:23:24 -0300

    ajustes

--- components/Flashcards.js
+++ components/Flashcards.js
@@ -1,131 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View, Animated, TouchableOpacity, TextInput, Keyboard } from 'react-native';
-import FlipCard from 'react-native-flip-card'
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView} from 'react-native';
 import { FontAwesome } from '@expo/vector-icons'
+import Baralho from './Baralho'
 
 export default class Flashcards extends React.Component {
 
     state = {
-        flip: false,
         title: 'Titulo FlashCards',
         pergunta: 'Sera? ',
         resposta: 'Sim!',
-        edit: false
     }
 
-    componentWillMount() {
-        this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide);
-    }
-
-    componentWillUnmount() {        
-        this.keyboardDidHideListener.remove();
-    }
-
-    _keyboardDidHide = () => {
-        this.setState({edit: false})
-    }
-
-    onFlip = () => {
-        this.setState(function (state, props) {
-            return {
-                flip: state.flip === true ? false : true
-            }
-        });
-    }
-
-    onEdit = () => {
-        this.setState(function (state, props) {
-            return {
-                edit: state.edit === true ? false : true
-            }
-        });
-    }
+    setPergunta = (pergunta) => this.setState({ pergunta })
+    setResposta = (resposta) => this.setState({ resposta })
 
     render() {
 
         return (
             <View style={styles.container}>
-                <View style={styles.containerFlip}>
-                    <FlipCard style={styles.flipCard}
-                        friction={6}
-                        perspective={1000}
-                        flipHorizontal={true}
-                        flipVertical={false}
-                        flip={this.state.flip}
-                        clickable={false}
-                    >
-                        
-                        <View style={styles.baralho}>
-                            <View style={styles.editContainerButton}>
-                                <TouchableOpacity onPress={this.onEdit} style={styles.editButton}>
-                                    <FontAwesome name='remove' size={30} color={'#3b3b3b'} />
-                                </TouchableOpacity>
-
-                                <Text style={styles.titleBaralho}>Front</Text>
-
-                                <TouchableOpacity onPress={this.onEdit} style={styles.editButton}>
-                                    <FontAwesome name='edit' size={30} color={'#3b3b3b'} />
-                                </TouchableOpacity>
-                            </View> 
-
-                            {this.state.edit === false &&
-                                <Text style={styles.text} onPress={this.onEdit}>
-                                    {this.state.pergunta}
-                                </Text>
-                            }
-                            {this.state.edit === true &&
-                                <TextInput
-                                    style={styles.textInput}
-                                    onChangeText={(pergunta) => this.setState({ pergunta })}
-                                    value={this.state.pergunta}
+                
+                <View style={styles.containerFlip} >
+                    <ScrollView horizontal={true} pagingEnabled={true} scrollsToTop={false} showsHorizontalScrollIndicator={false}>
+                            <Baralho 
+                                pergunta={this.state.pergunta} 
+                                setPergunta={this.setPergunta} 
+                                resposta={this.state.resposta}
+                                setResposta={this.setResposta} 
                                 />
-                            }
 
-                            <View style={styles.rotateContainerButton}>
-                                <TouchableOpacity onPress={this.onFlip} style={styles.rotateButton}>
-                                    <FontAwesome name='rotate-right' size={30} color={'#3b3b3b'} />
-                                </TouchableOpacity>    
-                            </View> 
-                        </View>
-                        <View style={styles.baralho}>
-                            <View style={styles.editContainerButton}>
-                                <TouchableOpacity onPress={this.onEdit} style={styles.editButton}>
-                                    <FontAwesome name='remove' size={30} color={'#3b3b3b'} />
-                                </TouchableOpacity>
-
-                                <Text style={styles.titleBaralho}>Back</Text>
-
-                                <TouchableOpacity onPress={this.onEdit} style={styles.editButton}>
-                                    <FontAwesome name='edit' size={30} color={'#3b3b3b'} />
-                                </TouchableOpacity>
-                            </View> 
-
-                            {this.state.edit === false &&
-                                <Text style={styles.text} onPress={this.onEdit}>
-                                    {this.state.resposta}
-                                </Text>
-                            }
-                            {this.state.edit === true &&
-                                <TextInput
-                                    style={styles.textInput}
-                                    onChangeText={(resposta) => this.setState({ resposta })}
-                                    value={this.state.resposta}
+                                <Baralho 
+                                pergunta={this.state.pergunta} 
+                                setPergunta={this.setPergunta} 
+                                resposta={this.state.resposta}
+                                setResposta={this.setResposta} 
                                 />
-                            }
 
-                            <View style={styles.rotateContainerButton}>
-                                <TouchableOpacity onPress={this.onFlip} style={styles.rotateButton}>
-                                    <FontAwesome name='rotate-left' size={30} color={'#3b3b3b'} />
-                                </TouchableOpacity>
-                            </View> 
-                        </View>
+                                <Baralho 
+                                pergunta={this.state.pergunta} 
+                                setPergunta={this.setPergunta} 
+                                resposta={this.state.resposta}
+                                setResposta={this.setResposta} 
+                                />
 
-                    </FlipCard>
+                    </ScrollView>
                 </View>
 
                 <View style={styles.footer}>
 
-                    <TouchableOpacity onPress={this.onFlip}>
+                    <TouchableOpacity>
                         <FontAwesome name='play-circle' size={60} style={styles.footerButtom} />
                     </TouchableOpacity>   
 
@@ -136,7 +58,7 @@ export default class Flashcards extends React.Component {
                         </Text>
                     </Text>
 
-                    <TouchableOpacity onPress={this.onFlip}>
+                    <TouchableOpacity>
                         <FontAwesome name='plus-circle' size={60} style={styles.footerButtom} />
                     </TouchableOpacity>   
 
@@ -155,37 +77,6 @@ const styles = StyleSheet.create({
     containerFlip: {
         flex: 5,
         backgroundColor: '#fff',
-        alignItems: 'center',
-    },
-    flipCard: {
-        borderColor: '#fff',
-    },
-    baralho: {
-        flex: 1,
-        width: 250,
-        backgroundColor: '#fff37a',
-        borderRadius: 7,
-        padding: 10,
-        margin: 60,
-        marginBottom: 30,
-        elevation: 2
-    },
-    rotateContainerButton: {
-        flex: 1,
-        flexDirection: 'column-reverse',
-        alignItems: 'flex-end',
-    },
-    rotateButton: {
-        width: 30,
-        height: 30
-    },
-    editContainerButton: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    editButton: {
-        width: 30,
-        height: 30
     },
     footer: {
         flex: 1,
